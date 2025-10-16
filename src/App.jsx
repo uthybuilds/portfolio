a1ed60990274b71a40a2dc1532004c4f02c3f2f6
BGC <uthmanajanaku@gmail.com>
2025-10-16 22:04:58 +0100

    feat added: about page

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Projects from "./components/Projects";
 import TechStack from "./components/TechStack";
 import Footer from "./components/Footer";
 import ContactSection from "./components/ContactSection";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navigation />
       <Hero />
+      <About />
       <Projects />
       <ContactSection />
       <TechStack />
